3bbcb034eb33eb9399026ff9d542156b71a196e6
Ahmed bilal <87586234+ahmedbilal205@users.noreply.github.com>
2023-03-05 14:45:42 -0500

    Update AdUnitIds.kt

--- app/src/main/java/com/anb/offlinetranslator/admob/AdUnitIds.kt
+++ app/src/main/java/com/anb/offlinetranslator/admob/AdUnitIds.kt
@@ -6,5 +6,5 @@ import com.anb.offlinetranslator.utils.AppConfig
 object AdUnitIds {
     var APP_OPEN_ID =MyApplication.TinyDB.getString(AppConfig.APP_OPEN) // test
 
-//    const val APP_OPEN_ID ="ca-app-pub-1067149317863301/2180196424" //Prod
+//    
 }
